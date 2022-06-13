09e3fd26cf7eb74d51b3d5dde3fe213d2f3b075c
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-06-13 18:41:10 +0900

    chore: completed tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js'
 
 const App: Component = () => {
-  return <div>some</div>
+  return <div class="text-lime-500 bg-red-500">some</div>
 }
 
 export default App
